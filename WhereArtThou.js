8fd4482f0a8f40715e3f3aa194cbb354745fbf35
Bakari Holmes <vocaljazz32@gmail.com>
2016-02-15 17:24:14 -0800

    added solution to object matching algorithm challenge

--- WhereArtThou.js
+++ WhereArtThou.js
@@ -26,3 +26,4 @@ function where(collection, source)
 }
 
 where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
