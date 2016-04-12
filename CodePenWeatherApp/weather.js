9091cf7f328d0af991998c49b0a3608d2f73dfc0
Bakari Holmes <vocaljazz32@gmail.com>
2016-04-12 13:31:28 -0700

    added Codepen weather app

---
+++ CodePenWeatherApp/weather.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+  var long;
+  var lat;
+  var fTemp;
+  var cTemp;
+  var img;
+  
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      
+      
+      // call open weather API passing in Geolocation
+      var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=40fb85a3b3caf562d1352ebe0fe0940d';
+      
+      
+      $.getJSON(api, function(data){
+        // pulling weather data from JSON
+        img = data.weather[0].icon;
+        var weatherType = data.weather[0].description;
+        var kTemp = data.main.temp;
+        // conversion to Celsius
+        cTemp = (kTemp-273).toFixed(1);
+        var tempSwap = true;
+        var windSpeed = data.wind.speed;
+        var city = data.name;
+        // converting to Fahrenheit
+        fTemp = ((9/5)*kTemp-459.67).toFixed(1);
+        // displaying pulled values
+        //console.log(weatherType);
+        //console.log(kTemp);
+        //console.log(windSpeed);
+        //console.log(city);
+        var icon = 'http://openweathermap.org/img/w/'+img+'.png';
+        $("#city").html(city);
+        $("#temp").html(fTemp + "&#8457;");
+        $("#temp").click(function(){
+          if(tempSwap){
+            $("#temp").html(cTemp + " &#8451;");
+            tempSwap=false;
+          }
+          else {
+            $("#temp").html(fTemp + "&#8457;");
+            tempSwap=true;
+          }
+        });
+        $("#weatherType").html(weatherType);
+        $("#wind").html(windSpeed);
+        //$("#icon").html(icon);
+        $("#weatherType").append("<img id='theImg'src="+icon+">");
+        console.log(api);
+      });
+      
+      //$("#data").html("latitude: " + lat + "<br>longitude: " + long);
+    });
+  }
+  
+  
+});
