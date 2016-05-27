a7c7f690339d2a6203f80f01a80759be0ad3014e
Bakari Holmes <vocaljazz32@gmail.com>
2016-05-27 13:17:26 -0700

    Used ip api to get geolocation
    Old method deprecated

--- CodePenWeatherApp/weather.js
+++ CodePenWeatherApp/weather.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
-  var long;
   var lat;
+  var long;
   var fTemp;
   var cTemp;
   var img;
-  
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-      
-      
-      // call open weather API passing in Geolocation
+  $.getJSON('http://ip-api.com/json', function(data){
+    lat = data.lat;
+    long = data.lon;
+    // call open weather API passing in Geolocation
       var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=40fb85a3b3caf562d1352ebe0fe0940d';
       
       
@@ -24,6 +20,7 @@ $(document).ready(function(){
         cTemp = (kTemp-273).toFixed(1);
         var tempSwap = true;
         var windSpeed = data.wind.speed;
+        
         var city = data.name;
         // converting to Fahrenheit
         fTemp = ((9/5)*kTemp-459.67).toFixed(1);
@@ -34,7 +31,7 @@ $(document).ready(function(){
         //console.log(city);
         var icon = 'http://openweathermap.org/img/w/'+img+'.png';
         $("#city").html(city);
-        $("#temp").html(fTemp + "&#8457;");
+        $("#temp").html(fTemp + " &#8457;");
         $("#temp").click(function(){
           if(tempSwap){
             $("#temp").html(cTemp + " &#8451;");
@@ -46,15 +43,16 @@ $(document).ready(function(){
           }
         });
         $("#weatherType").html(weatherType);
-        $("#wind").html(windSpeed);
+        windSpeed = (2.237*windSpeed).toFixed(1);
+        $("#wind").html("Windspeed: " +windSpeed+" mph");
         //$("#icon").html(icon);
         $("#weatherType").append("<img id='theImg'src="+icon+">");
         console.log(api);
       });
       
       //$("#data").html("latitude: " + lat + "<br>longitude: " + long);
+  });
+   
+      
     });
-  }
   
-  
-});
